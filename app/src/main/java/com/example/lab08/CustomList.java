f1b8a65927a7f185be6ced8ae7e86bca8d66b904
parallelXYZ <lnone67@gmail.com>
2025-10-28 17:28:05 -0600

    implemented hasCity and test passed

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,7 +17,6 @@ public class CustomList {
     
 
     // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
-
+    public boolean hasCity(City city) { return cities.contains(city); }
     // test comment
 }
